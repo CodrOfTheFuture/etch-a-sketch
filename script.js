901592252f774641add857b6a2e354530f7e4d09
CodrOfTheFuture <165679645+CodrOfTheFuture@users.noreply.github.com>
2024-11-14 10:12:24 -0500

    starting

---
+++ script.js
@@ -0,0 +1,8 @@
+let divs = []; // Array to hold the divs
+
+for (let i = 0; i < 16; i++) {
+    let div = document.createElement("div"); // Create a div element
+    divs.push(div); // Add it to the array
+}
+
+console.log(divs); // This will log an array of 16 div elements
