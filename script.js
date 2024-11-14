da8b35bc0fb1c1e8e07b214a0fd4e2d545ecffdc
CodrOfTheFuture <165679645+CodrOfTheFuture@users.noreply.github.com>
2024-11-14 10:13:08 -0500

    sigma

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
-let divs = []; // Array to hold the divs
+let divs = [];
+let container = document.createElement("div");
 
-for (let i = 0; i < 16; i++) {
-    let div = document.createElement("div"); // Create a div element
-    divs.push(div); // Add it to the array
+container.setAttribute("style", "display: flex; flex-wrap: wrap;  width: 256px;");
+
+for (let i = 0; i < 256; i++) {
+    let div = document.createElement("div");
+    div.setAttribute("style", "background-color: black; width: 15px; height: 15px;");
+    divs.push(div); 
 }
 
-console.log(divs); // This will log an array of 16 div elements
+for (let i = 0; i < 16; i++) {
+    let row = document.createElement("div");
+    row.setAttribute("style", "display: flex; ");
+
+    for (let j = 0; j < 16; j++) {
+        row.appendChild(divs[i * 16 + j]);
+    }
+    container.appendChild(row);
+}
+
+// Append the container to the body
+document.body.appendChild(container);
+
+console.log(divs);
