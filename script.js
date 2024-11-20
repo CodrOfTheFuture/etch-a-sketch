a616b24b81d560b48f09b6ac1122a37e67aa0cfc
CodrOfTheFuture <165679645+CodrOfTheFuture@users.noreply.github.com>
2024-11-20 11:56:53 -0500

    trying to add custom sizes using btn, NOT WORKING :CRY

--- script.js
+++ script.js
@@ -1,25 +1,51 @@
 let divs = [];
 let container = document.createElement("div");
+let mk_btn = document.createElement("div")
 
-container.setAttribute("style", "display: flex; flex-wrap: wrap;  width: 256px;");
+size = 16
 
-for (let i = 0; i < 256; i++) {
+container.setAttribute("style", "display: flex; flex-wrap: wrap; width: 256px; border: solid; border-color:black; justify-content:center; align-items:center;margin-left:800px;margin-top:300px;");
+mk_btn.setAttribute("style", "height:70px; width: 100px; border:solid; border-color:black;")
+
+// Create 256 divs and store them in the divs array
+for (let i = 0; i < (size * size); i++) {
     let div = document.createElement("div");
-    div.setAttribute("style", "background-color: black; width: 15px; height: 15px;");
-    divs.push(div); 
+    div.setAttribute("style", "background-color: white; width: 15px; height: 15px;");
+    divs.push(div);
 }
 
-for (let i = 0; i < 16; i++) {
+mk_btn.addEventListener("click", () => {
+    size = prompt("Choose a size 16 to 100")
+    if (size < 16 || size > 100){
+        alert("Invalid input")
+    }
+})
+
+
+// Create rows and append divs into rows
+for (let i = 0; i < size; i++) {
     let row = document.createElement("div");
     row.setAttribute("style", "display: flex; ");
 
-    for (let j = 0; j < 16; j++) {
-        row.appendChild(divs[i * 16 + j]);
+    for (let j = 0; j < size; j++) {
+        row.appendChild(divs[i * size + j]);
     }
     container.appendChild(row);
 }
 
 // Append the container to the body
 document.body.appendChild(container);
+document.body.appendChild(mk_btn);
+
 
 console.log(divs);
+
+// Add event listener to each div for mouseover
+divs.forEach(div => {
+    div.addEventListener('mouseover', () => {
+        // Only change the background-color, keep other styles intact
+        div.style.backgroundColor = 'blue';
+        console.log('Hovered over a div!');
+    });
+
+});
